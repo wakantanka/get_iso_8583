d49d5da793194a82bdfcd649c19879e3f82e8723
U-WIRECARD\jan.wahler <jan.wahler@GCKBJ5J.wirecard.lan>
2014-01-20 10:22:13 +0100

    read bitmap

---
+++ src/test/java/de/wirecard/acqp/twi/MsgUtils.java
@@ -0,0 +1,60 @@
+package de.wirecard.acqp.twi;
+
+import java.io.ByteArrayOutputStream;
+
+import org.jpos.iso.ISOException;
+import org.jpos.iso.LeftPadder;
+
+public final class MsgUtils {
+
+	public static String GetMTI(String input) {
+		return stripFs(input.substring(0, 8));
+	}
+
+	public static String GetBitMap(String input) {
+		// System.out.println(ISOUtil.trimf(testdata.substring(8, 24)));
+		return input.substring(8, 24);
+	}
+
+	private static byte[] decodeNibbleHex(String input) {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		char[] chars = input.toCharArray();
+		for (int i = 0; i < chars.length - 1; i += 2) {
+			char[] bChars = new char[2];
+			bChars[0] = chars[i];
+			bChars[1] = chars[(i + 1)];
+			int val = Integer.decode("0x" + new String(bChars)).intValue();
+			baos.write((byte) val);
+		}
+		return baos.toByteArray();
+	}
+
+	private static String stripFs(String input) {
+		StringBuilder result = new StringBuilder();
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) != 'F' && i % 2 == 1)
+				result.append(input.charAt(i));
+		}
+		return result.toString();
+	}
+
+	static String hexStringToBinaryString(String HexString) throws ISOException {
+		StringBuilder sb = new StringBuilder();
+		char[] chars = HexString.toCharArray();
+
+		for (int i = 0; i < chars.length - 1; i++) {
+			String hex = "" + chars[i] + chars[i + 1];
+			i++;
+			sb.append(hexToBinary(hex));
+
+		}
+
+		return sb.toString();
+	}
+
+	static String hexToBinary(String Hex) throws ISOException {
+		int i = Integer.parseInt(Hex, 16);
+		String Bin = Integer.toBinaryString(i);
+		return LeftPadder.ZERO_PADDER.pad(Bin, 8);
+	}
+}
---
+++ src/test/java/de/wirecard/acqp/twi/SplitMsgPartsTest.java
@@ -0,0 +1,93 @@
+package de.wirecard.acqp.twi;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.BitSet;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.BinaryCodec;
+import org.apache.commons.codec.binary.Hex;
+import org.jpos.iso.ISOException;
+import org.jpos.util.FrozenLogEvent;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SplitMsgPartsTest {
+
+	
+	private static String testdata;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		StringBuilder sb = new StringBuilder();
+
+		File testdataFile = new File("./resources/testdata.ac");
+		FileInputStream in = new FileInputStream(testdataFile);
+		InputStreamReader iSr = new InputStreamReader(in, "UTF-8");
+
+		
+		char[] chars = new char[4 * 1024];
+		int len;
+		while ((len = iSr.read(chars)) >= 0) {
+			sb.append(chars, 0, len);
+		}
+		iSr.close();
+		 testdata = sb.toString();
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+		
+	@Test
+	public void testSeperateGetMTI() {
+//		assertEquals("F0F1F0F0", MsgUtils.GetMTI(testdata));
+		assertEquals("0100", MsgUtils.GetMTI(testdata));
+	}
+	
+	@Test
+	public void testSeperateGetBitMap() throws DecoderException {
+		assertEquals("723C440188E18008", MsgUtils.GetBitMap(testdata));
+//		
+//		char[] chars = "723C440188E18008".toCharArray();
+//		byte[] bytes = Hex.decodeHex(chars);
+//		System.out.print(BinaryCodec.toAsciiString(bytes));
+
+		
+//		for (int i = 0; i < bytes.length; i++) {
+//			System.out.print(bytes[i]);
+//		}
+	}
+
+//	@Test
+	public void testBitmap() {
+//		BitSet bitmap = new BitSet();
+//		bitmap.
+//		ISOBitMap(int n, java.util.BitSet v) 
+	}
+	
+	@Test
+	public void testBinBitmap() throws ISOException {
+		assertEquals("01110010", MsgUtils.hexToBinary("72"));
+		assertEquals("0111001000111100", MsgUtils.hexStringToBinaryString("723C"));
+		assertEquals("0111001000111100010001000000000110001000111000011000000000001000", MsgUtils.hexStringToBinaryString("723C440188E18008"));
+		
+       char[] bitmapBin = MsgUtils.hexStringToBinaryString("723C440188E18008").toCharArray();
+for (int i = 0; i < bitmapBin.length; i++) {
+	if (bitmapBin[i] == '1')
+		System.out.println( i+1);
+}
+ 
+	}
+	
+	
+}
