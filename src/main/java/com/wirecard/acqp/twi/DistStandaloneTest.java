cc39946c8b02a4cf3c9877b9a70a2e041b3155b5
bs <you@example.com>
2014-01-29 10:51:48 +0100

    assembly

--- src/main/java/com/wirecard/acqp/twi/DistStandaloneTest.java
+++ src/main/java/com/wirecard/acqp/twi/DistStandaloneTest.java
@@ -13,13 +13,21 @@ import org.jpos.iso.ISOMsg;
 public class DistStandaloneTest {
 
 	/**
-	 * @param args
+	 * @param args none
+	 * {@code} java -cp "target/distribution/msgaccessory/*;" com.wirecard.acqp.twi.DistStandaloneTest
+	 * {@code} java -cp "target/distribution/msgaccessory;target/distribution/msgaccessory/lib/*;" com.wirecard.acqp.twi.DistStandaloneTest
+	 * {@code} java -cp "lib/*;" com.wirecard.acqp.twi.DistStandaloneTest
 	 */
 	public static void main(String[] args) {
-		ISOMsg isoMsg = new ISOMsg();
 		try {
+			ISOMsg isoMsg = new ISOMsg();
 			isoMsg.setMTI("0100");
+			System.out.println("DistStandaloneTest ok");
 		} catch (ISOException e) {
+			System.err.println("DistStandaloneTest failed");
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.err.println("DistStandaloneTest failed");
 			e.printStackTrace();
 		}
 
