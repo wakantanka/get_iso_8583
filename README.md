61a3b3d504781d9ab125ad522d60174bd552c0bd
bs <you@example.com>
2014-02-10 15:01:54 +0100

    jmeter function dist

--- README.md
+++ README.md
@@ -1,11 +1,5 @@
 
 
-uses customized dataformat  
+install 
+cp -R target/distribution/interchange-msg-accessory-distribution/interchange-msg-accessory/* ~/dev/apache-jmeter-2.9/
 
-not implemendet yet
-
-61.14
-48 (combinations unclear)
-JCB
-ExceptionHandling
-XML output
--- src/main/java/com/wirecard/acqp/two/jmeter/functions/ReadInterchangeMsgField.java
+++ src/main/java/com/wirecard/acqp/two/jmeter/functions/ReadInterchangeMsgField.java
@@ -17,6 +17,8 @@ import org.apache.jmeter.samplers.Sampler;
 import org.apache.jmeter.threads.JMeterVariables;
 import org.jpos.iso.ISOException;
 
+import com.wirecard.acqp.two.MsgAccessoryImpl;
+
 /**
  * @author wirecard
  *
@@ -49,29 +51,18 @@ public class ReadInterchangeMsgField extends AbstractFunction {
     @Override
     public synchronized String execute(SampleResult previousResult, Sampler currentSampler)
             throws InvalidVariableException {
+    	System.out.println("############################# EXEC ReadInterchangeMsgField #############################");
+    	System.out.println("############################# EXEC " + ((CompoundVariable) values[0]).execute().trim()  + " #############################");
+    	System.out.println("############################# EXEC " + ((CompoundVariable) values[1]).execute().trim()  + " #############################");
+    	System.out.println("############################# EXEC " + ((CompoundVariable) values[2]).execute().trim()  + " #############################");
 
         JMeterVariables vars = getVariables();
 
-        Double sum = 0D;
-        String varName = ((CompoundVariable) values[values.length - 1]).execute().trim();
 
-        for (int i = 0; i < values.length - 1; i++) {
-            sum += Double.parseDouble(((CompoundVariable) values[i]).execute());
-        }
-
-        try {
-            sum += Double.parseDouble(varName);
-            varName = null; // there is no variable name
-        } catch (NumberFormatException ignored) {
-        }
-
-        String totalString = Double.toString(sum);
-        if (vars != null && varName != null && varName.length() > 0) {// vars will be null on TestPlan
-            vars.put(varName, totalString);
-        }
+        String totalString = null;
         
     	try {
-			System.out.println(com.wirecard.acqp.two.MsgAccessoryImpl.readFieldValue( "16010200D600000079542500000000000000000000000400F22464810CE08016000000400000000010400601FFFFFF0013000000000000002238012812282961059615110780080401205906428104F4F0F2F8F1F2F6F1F0F5F9F6F9F5F0F1F1F583F0F0F0F0F0F0F1F1F0F0F383F0F0F0F0F0F0F1404040C6C3C240E28899A340D5819485404040404040404040404040C6C3C240E28899A340D3968340E4C10978050900000005104000000000000000041402859374059607A0000000002501010000000000000000000000000000000000000000", "VISA", "62.2" ));
+    		totalString = MsgAccessoryImpl.readFieldValue(((CompoundVariable) values[0]).execute().trim(), ((CompoundVariable) values[1]).execute().trim(), ((CompoundVariable) values[2]).execute().trim() );
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
