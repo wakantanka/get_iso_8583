b2492995c67bb8e863e2566846a1b3662b4e9440
bs <you@example.com>
2014-02-11 14:36:32 +0100

    simpleSample IT

--- src/main/java/com/wirecard/acqp/two/jmeter/samplers/InterchangeAccessorySampler.java
+++ src/main/java/com/wirecard/acqp/two/jmeter/samplers/InterchangeAccessorySampler.java
@@ -5,145 +5,154 @@ import org.apache.jmeter.samplers.Entry;
 import org.apache.jmeter.samplers.Interruptible;
 import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.testbeans.TestBean;
-/**
-*
-* @author Wirecard AG (c) 2014. All rights reserved.
-*/
 
-public class InterchangeAccessorySampler
-        extends AbstractSampler implements TestBean , Interruptible {
+/**
+ * 
+ * @author Wirecard AG (c) 2014. All rights reserved.
+ */
+
+public class InterchangeAccessorySampler extends AbstractSampler implements
+		TestBean, Interruptible {
 
 	private static final long serialVersionUID = 6455497354844447057L;
 	public static final String IS_SUCCESSFUL = "SUCCESFULL";
-    public static final String RESPONSE_CODE = "RESPONSE_CODE";
-    public static final String RESPONSE_MESSAGE = "RESPONSE_MESSAGE";
-    public static final String RESPONSE_DATA = "RESPONSE_DATA";
-    public static final String REQUEST_DATA = "REQUEST_DATA";
-    public static final String RESPONSE_TIME = "RESPONSE_TIME";
-    public static final String LATENCY = "LATENCY";
-    public static final String IS_WAITING = "WAITING";
+	public static final String RESPONSE_CODE = "RESPONSE_CODE";
+	public static final String RESPONSE_MESSAGE = "RESPONSE_MESSAGE";
+	public static final String RESPONSE_DATA = "RESPONSE_DATA";
+	public static final String REQUEST_DATA = "REQUEST_DATA";
+	public static final String RESPONSE_TIME = "RESPONSE_TIME";
+	public static final String LATENCY = "LATENCY";
+	public static final String IS_WAITING = "WAITING";
 
-    public SampleResult sample(Entry e) {
-        SampleResult res;
-        if (isSimulateWaiting()) {
-            res = new SampleResult();
-            res.sampleStart();
-            try {
-                Thread.sleep(getResponseTime());
-            } catch (InterruptedException ignored) {
-            }
-            res.sampleEnd();
-        } else {
-            res = new SampleResult(System.currentTimeMillis(), getResponseTime());
-        }
+	public SampleResult sample(Entry e) {
+		SampleResult res;
+		if (isSimulateWaiting()) {
+			res = new SampleResult();
+			res.sampleStart();
+			try {
+				Thread.sleep(getResponseTime());
+			} catch (InterruptedException ignored) {
+			}
+			res.sampleEnd();
+		} else {
+			res = new SampleResult(System.currentTimeMillis(),
+					getResponseTime());
+		}
 
-        res.setSampleLabel(getName());
+		res.setSampleLabel(getName());
 
-        // source data
-        res.setSamplerData(getRequestData());
+		// source data
+		res.setSamplerData(getRequestData());
 
-        // response code
-        res.setResponseCode(getResponseCode());
-        res.setResponseMessage(getResponseMessage());
-//        res.setSuccessful(isSuccessfull());
+		// response code
+		res.setResponseCode(getResponseCode());
+		res.setResponseMessage(getResponseMessage());
+		res.setSuccessful(isSuccessfull());
 
-        // responde data
+		// responde data
+		// res.setResponseData(getResponseData().getBytes());
+
+		// res.setLatency(getLatency());
+		res.setDataEncoding("UTF-8");
+//		res.setSamplerData("samplerRequsData");
+        res.setResponseData("response", null);
         res.setDataType(SampleResult.TEXT);
-  res.setResponseData(getResponseData().getBytes());
+        res.setResponseOK();
+        res.sampleEnd();
+        
+		return res;
+	}
 
-//        res.setLatency(getLatency());
+	public void setSuccessful(boolean selected) {
+		// selected=true;
+		setProperty(IS_SUCCESSFUL, selected);
+	}
 
-        return res;
-    }
+	public void setSimulateWaiting(boolean selected) {
+		setProperty(IS_WAITING, selected);
+	}
 
-    public void setSuccessful(boolean selected) {
-        setProperty(IS_SUCCESSFUL, selected);
-    }
+	public void setResponseCode(String text) {
+		setProperty(RESPONSE_CODE, text);
+	}
 
-    public void setSimulateWaiting(boolean selected) {
-        setProperty(IS_WAITING, selected);
-    }
+	public void setResponseMessage(String text) {
+		setProperty(RESPONSE_MESSAGE, text);
+	}
 
-    public void setResponseCode(String text) {
-        setProperty(RESPONSE_CODE, text);
-    }
+	public void setResponseData(String text) {
+		setProperty(RESPONSE_DATA, text);
+	}
 
-    public void setResponseMessage(String text) {
-        setProperty(RESPONSE_MESSAGE, text);
-    }
+	public void setRequestData(String text) {
+		setProperty(REQUEST_DATA, text);
+	}
 
-    public void setResponseData(String text) {
-        setProperty(RESPONSE_DATA, text);
-    }
+	/**
+	 * @return the successfull
+	 */
+	public boolean isSuccessfull() {
+		return true;
+		// getPropertyAsBoolean(IS_SUCCESSFUL);
+	}
 
-    public void setRequestData(String text) {
-        setProperty(REQUEST_DATA, text);
-    }
+	public boolean isSimulateWaiting() {
+		return getPropertyAsBoolean(IS_WAITING);
+	}
 
-    /**
-     * @return the successfull
-     */
-    public boolean isSuccessfull() {
-        return getPropertyAsBoolean(IS_SUCCESSFUL);
-    }
+	/**
+	 * @return the responseCode
+	 */
+	public String getResponseCode() {
+		return getPropertyAsString(RESPONSE_CODE);
+	}
 
-    public boolean isSimulateWaiting() {
-        return getPropertyAsBoolean(IS_WAITING);
-    }
+	/**
+	 * @return the responseMessage
+	 */
+	public String getResponseMessage() {
+		return getPropertyAsString(RESPONSE_MESSAGE);
+	}
 
-    /**
-     * @return the responseCode
-     */
-    public String getResponseCode() {
-        return getPropertyAsString(RESPONSE_CODE);
-    }
+	/**
+	 * @return the responseData
+	 */
+	public String getResponseData() {
+		return getPropertyAsString(RESPONSE_DATA);
+	}
 
-    /**
-     * @return the responseMessage
-     */
-    public String getResponseMessage() {
-        return getPropertyAsString(RESPONSE_MESSAGE);
-    }
+	public String getRequestData() {
+		return getPropertyAsString(REQUEST_DATA);
+	}
 
-    /**
-     * @return the responseData
-     */
-    public String getResponseData() {
-        return getPropertyAsString(RESPONSE_DATA);
-    }
+	public int getResponseTime() {
+		int time = 0;
+		try {
+			time = Integer.valueOf(getPropertyAsString(RESPONSE_TIME));
+		} catch (NumberFormatException ignored) {
+		}
+		return time;
+	}
 
-    public String getRequestData() {
-        return getPropertyAsString(REQUEST_DATA);
-    }
+	public int getLatency() {
+		int time = 0;
+		try {
+			time = Integer.valueOf(getPropertyAsString(LATENCY));
+		} catch (NumberFormatException ignored) {
+		}
+		return time;
+	}
 
-    public int getResponseTime() {
-        int time = 0;
-        try {
-            time = Integer.valueOf(getPropertyAsString(RESPONSE_TIME));
-        } catch (NumberFormatException ignored) {
-        }
-        return time;
-    }
+	public void setResponseTime(String time) {
+		setProperty(RESPONSE_TIME, time);
+	}
 
-    public int getLatency() {
-        int time = 0;
-        try {
-            time = Integer.valueOf(getPropertyAsString(LATENCY));
-        } catch (NumberFormatException ignored) {
-        }
-        return time;
-    }
+	public void setLatency(String time) {
+		setProperty(LATENCY, time);
+	}
 
-    public void setResponseTime(String time) {
-        setProperty(RESPONSE_TIME, time);
-    }
-
-    public void setLatency(String time) {
-        setProperty(LATENCY, time);
-    }
-
-    public boolean interrupt() {
-        Thread.currentThread().interrupt();
-        return true;
-    }
+	public boolean interrupt() {
+		Thread.currentThread().interrupt();
+		return true;
+	}
 }
